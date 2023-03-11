988b37ef2ad11bfc27b9829a09a82debd4dc98b6
Andrew Powers-Holmes <aholmes@omnom.net>
2023-03-11 17:24:01 +1100

    add some utils to common

--- src/cogs/common/__init__.py
+++ src/cogs/common/__init__.py
@@ -1,3 +1,11 @@
+from typing import Union
+
+from disnake import DMChannel, GroupChannel, TextChannel, Thread
+
 from .upscaler import Upscaler
 
-__all__ = "Upscaler"
+from . import utils
+
+MessageChannel = Union[TextChannel, Thread, DMChannel, GroupChannel]
+
+__all__ = "Upscaler", "MessageChannel", "utils"
