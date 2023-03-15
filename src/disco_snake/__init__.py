0cf24f7a5c6d29a20f66129fdf68a8901ed15177
Andrew Powers-Holmes <aholmes@omnom.net>
2023-03-15 16:25:05 +1100

    rework some consts

--- src/disco_snake/__init__.py
+++ src/disco_snake/__init__.py
@@ -9,20 +9,21 @@ from pathlib import Path
 
 from helpers.misc import get_package_root
 
-LOGDIR_PATH = Path.cwd().joinpath("logs")
-DATADIR_PATH = Path.cwd().joinpath("data")
-
 LOG_FORMAT = "%(color)s[%(levelname)1.1s %(asctime)s][%(name)s][%(module)s:%(funcName)s:%(lineno)d]%(end_color)s %(message)s"
 
-# LOGDIR_PATH = Path(__file__).parent.parent.parent.joinpath("logs")
-# DATADIR_PATH = Path(__file__).parent.parent.parent.joinpath("data")
+PACKAGE_ROOT = get_package_root()
+
+COGDIR_PATH = PACKAGE_ROOT.joinpath("cogs")
+# LOGDIR_PATH = PACKAGE_ROOT.parent.joinpath("logs")
+# DATADIR_PATH = PACKAGE_ROOT.parent.joinpath("data")
+
+LOGDIR_PATH = Path.cwd().joinpath("logs")
+DATADIR_PATH = Path.cwd().joinpath("data")
 
 CONFIG_PATH = DATADIR_PATH.joinpath("config.json")
 USERDATA_PATH = DATADIR_PATH.joinpath("userdata.json")
 BLACKLIST_PATH = DATADIR_PATH.joinpath("blacklist.json")
+MISCDATA_PATH = DATADIR_PATH.joinpath("misc")
 
-PACKAGE_ROOT = get_package_root()
-COGDIR_PATH = PACKAGE_ROOT.joinpath("cogs")
-EXTDIR_PATH = PACKAGE_ROOT.joinpath("extensions")
 
 PACKAGE = __package__.replace("_", "-")
