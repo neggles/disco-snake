1e8db6b1f488fff2121f2f54ba4d3ba4863b807c
Andrew Powers-Holmes <aholmes@omnom.net>
2023-05-15 01:14:56 +1000

    shimeji: remove kshjillion recursive imports

--- shimeji/__init__.py
+++ shimeji/__init__.py
@@ -7,44 +7,14 @@ __copyright__ = "Copyright 2022 hitomi-team"
 __path__ = __import__("pkgutil").extend_path(__path__, __name__)
 
 from shimeji.shimeji import ChatBot
-from shimeji.model_provider import (
-    ModelGenArgs,
-    ModelGenRequest,
-    ModelSampleArgs,
-    SukimaModel,
-    ModelProvider,
-    ModelSerializer,
-    ModelLogitBiasArgs,
-    ModelPhraseBiasArgs,
-    BaseModel,
-    EnmaModel,
-    TextSynthModel,
-)
-from shimeji.preprocessor import Preprocessor, MemoryPreprocessor, ContextPreprocessor
-from shimeji.postprocessor import Postprocessor, NewlinePrunerPostprocessor
-from shimeji.memory import (
-    Memory,
-    numpybin_to_str,
-    array_to_str,
-    str_to_numpybin,
-    cosine_distance,
-    memory_sort,
-    memory_context,
-)
-from shimeji.memory.providers import MemoryStore, PostgresMemoryStore
-from shimeji.util import (
-    TRIM_DIR_TOP,
-    TRIM_DIR_BOTTOM,
-    TRIM_DIR_NONE,
-    TRIM_TYPE_NEWLINE,
-    TRIM_TYPE_SENTENCE,
-    TRIM_TYPE_TOKEN,
-    INSERTION_TYPE_NEWLINE,
-    INSERTION_TYPE_SENTENCE,
-    INSERTION_TYPE_TOKEN,
-    split_into_sentences,
-    trim_newlines,
-    trim_sentences,
-    trim_tokens,
-    ContextEntry,
-)
+
+from shimeji import memory, model_provider, postprocessor, preprocessor, util
+
+__all__ = [
+    "ChatBot",
+    "memory",
+    "model_provider",
+    "postprocessor",
+    "preprocessor",
+    "util",
+]
