5cd67afbd14947cc8ad3cac342da20cf519c0763
Andrew Powers-Holmes <aholmes@omnom.net>
2023-05-21 14:14:13 +1000

    add some helper methods to discord db schema object, retcon global_name

--- src/db/discord.py
+++ src/db/discord.py
@@ -3,8 +3,10 @@ from typing import Annotated, Optional
 
 import disnake
 import sqlalchemy as sa
+from disnake.ext import commands
 from sqlalchemy.dialects import postgresql as pg
-from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy.ext.hybrid import hybrid_property
+from sqlalchemy.orm import Mapped, mapped_column, relationship, synonym_for
 
 from db.base import Base, BigIntPK, CreateTimestamp, Timestamp, UpdateTimestamp
 
@@ -27,13 +29,11 @@ class DiscordUser(Base):
     __table_args__ = (
         sa.PrimaryKeyConstraint("id"),
         sa.UniqueConstraint("username", "discriminator"),
-        sa.UniqueConstraint("global_name"),
     )
 
     id: Mapped[DiscordSnowflake] = mapped_column(primary_key=True)
     username: Mapped[DiscordName]
     discriminator: Mapped[int] = mapped_column(sa.Integer, server_default="0")
-    global_name: Mapped[Optional[str]] = mapped_column(sa.String)
 
     avatar: Mapped[Optional[str]] = mapped_column(sa.String(length=512))
     bot: Mapped[bool] = mapped_column(sa.Boolean)
@@ -53,33 +53,71 @@ class DiscordUser(Base):
     tos_accepted_at: Mapped[Timestamp]
     # tos_accept_msg: Mapped[]
 
+    @hybrid_property
+    def name(self) -> str:
+        if self.discriminator == 0:
+            return self.username
+        return f"{self.username}#{self.discriminator}"
+
+    @name.setter
+    def name(self, name: str, discriminator: int = 0) -> None:
+        """Set the user's name, optionally with discriminator.
+            Discriminator can be passed as a separate argument, or
+            as part of the name string.
+            Pass 0 as the discriminator to set the global name.
+
+
+        Args:
+            name (str): Discord user name with optional discriminator (e.g. "username#1234")
+            discriminator (int, optional, default=0): Discord user discriminator
+        """
+        if "#" in name:
+            username, discriminator = name.split("#")
+            discriminator = int(discriminator)
+        self.username = username
+        self.discriminator = discriminator
+
     @classmethod
     def from_discord(cls, user: disnake.User | disnake.Member) -> "DiscordUser":
+        if isinstance(user, disnake.Member):
+            return cls(
+                username=user.name,
+                discriminator=user.discriminator,
+                avatar=user.avatar.key if user.avatar else None,
+                bot=user.bot,
+                system=user.system,
+                flags=user.flags.value,
+                public_flags=user.public_flags.value,
+            )
         return cls(
             id=user.id,
             username=user.name,
             discriminator=user.discriminator,
-            global_name=getattr(user, "global_name", None),
             avatar=user.avatar.key if user.avatar else None,
             bot=user.bot,
             system=user.system,
-            flags=user.flags.value if hasattr(user, "flags") else None,
             public_flags=user.public_flags.value,
         )
 
+    async def to_discord(self, bot: commands.Bot) -> disnake.User:
+        user = await bot.fetch_user(self.id)
+        if user is None:
+            raise ValueError(f"User {self.id} not found on Discord")
 
-class UsernameHistory(Base):
-    __tablename__ = "username_history"
-    __table_args__ = (sa.PrimaryKeyConstraint("user_id", "timestamp"),)
 
-    user_id: Mapped[int] = mapped_column(sa.BigInteger, sa.ForeignKey("users.id", ondelete="CASCADE"))
-    user: Mapped[DiscordUser] = relationship(backref="username_history")
-    timestamp: Mapped[datetime] = mapped_column(
-        pg.TIMESTAMP(timezone=True, precision=2),
-        server_default=sa.func.current_timestamp(),
-        nullable=False,
-    )
-    username: Mapped[DiscordName] = mapped_column(nullable=False)
+## coming back to these later
+# class UsernameHistory(Base):
+#     __tablename__ = "username_history"
+#     __table_args__ = (sa.PrimaryKeyConstraint("user_id", "timestamp"),)
+
+#     user_id: Mapped[int] = mapped_column(sa.BigInteger, sa.ForeignKey("users.id", ondelete="CASCADE"))
+#     user: Mapped[DiscordUser] = relationship(backref="username_history")
+#     timestamp: Mapped[datetime] = mapped_column(
+#         pg.TIMESTAMP(timezone=True, precision=2),
+#         server_default=sa.func.current_timestamp(),
+#         nullable=False,
+#     )
+#     username: Mapped[DiscordName] = mapped_column(nullable=False)
 
 
 # class NicknameHistory(Base):
