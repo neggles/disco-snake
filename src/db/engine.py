eb5017500c6c0df5cda0ea892590fe94adf1c6b0
Andi Powers-Holmes <aholmes@omnom.net>
2025-12-17 19:26:53 +1100

    some db engine code structure tweaks, no functional change

--- src/db/engine.py
+++ src/db/engine.py
@@ -5,14 +5,14 @@ from typing import Any
 from pydantic_core import to_jsonable_python
 from sqlalchemy import Engine, create_engine
 from sqlalchemy.ext.asyncio import AsyncEngine, AsyncSession, async_sessionmaker, create_async_engine
-from sqlalchemy.orm import (
-    Session as DbSession,
-)
-from sqlalchemy.orm import (
-    sessionmaker,
-)
+from sqlalchemy.orm import Session as DbSession
+from sqlalchemy.orm import sessionmaker
 
 from disco_snake.settings import BotSettings, get_settings
+
+# Define type aliases for session makers
+type SessionType = async_sessionmaker[AsyncSession]
+type SyncSessionType = sessionmaker[DbSession]
 
 
 def json_serializer(obj: Any, *args, **kwargs) -> str:
@@ -20,6 +20,8 @@ def json_serializer(obj: Any, *args, **kwargs) -> str:
     return json.dumps(obj, *args, default=to_jsonable_python, ensure_ascii=False, **kwargs)
 
 
+# this is a kinda hacky way to make an effective module-level singleton, but it works
+# TODO: refactor to proper singleton classes
 @lru_cache(maxsize=1)
 def get_sync_engine() -> Engine:
     settings: BotSettings = get_settings()
@@ -28,6 +30,9 @@ def get_sync_engine() -> Engine:
         echo=settings.debug,
         json_serializer=json_serializer,
     )
+
+
+SyncSession: SyncSessionType = sessionmaker(get_sync_engine(), expire_on_commit=False)
 
 
 @lru_cache(maxsize=1)
@@ -40,8 +45,4 @@ def get_engine() -> AsyncEngine:
     )
 
 
-Session: async_sessionmaker[AsyncSession] = async_sessionmaker(get_engine(), expire_on_commit=False)
-type SessionType = async_sessionmaker[AsyncSession]
-
-SyncSession: sessionmaker[DbSession] = sessionmaker(get_sync_engine(), expire_on_commit=False)
-type SyncSessionType = sessionmaker[DbSession]
+Session: SessionType = async_sessionmaker(get_engine(), expire_on_commit=False)
